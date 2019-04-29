3fb37da3de91b5d4b37273eae0b4fae96d839d01
Camilo Lozano <camilolozanoarteaga@gmail.com>
2019-04-28 19:25:16 -0500

    initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  get fetchImages async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            // setState(() {
+            //   counter += 1;
+            // });
+            fetchImages;
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some imges!'),
+        ),
+      ),
+    );
+  }
+}
